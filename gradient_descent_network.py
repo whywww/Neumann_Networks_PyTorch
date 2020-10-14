fixed normalization bug

gradient_descent_network.py
@@ -22,9 +22,13 @@ class GradientDescentNet():
             self.load_checkpoints()
             self.start_epoch = args.load + 1
         
-        self.eta = torch.tensor(0.1, dtype=torch.float32, requires_grad=True, device=device)
-        self.opr = Operators(device=device)
-        self.optimizer = optim.Adam(self.resnet.parameters(), lr=args.lr, betas=(0.999, 0.999))
+        self.opr = Operators(args.height, args.width, 8, device=device)
+        self.eta = self.opr.estimate_eta().requires_grad_()
+        print(f'initial eta estimate: {self.eta:.6f}')
+#         self.eta = torch.tensor(0.00001, dtype=torch.float32, requires_grad=True, device=device)
+        self.optimizer = optim.Adam([{"params":self.resnet.parameters()},
+                                     {"params":self.eta}], 
+                                    lr=args.lr, betas=(0.999, 0.999))
         self.scheduler = optim.lr_scheduler.ExponentialLR(self.optimizer, gamma=0.97)
         self.criterionL2 = torch.nn.MSELoss().to(device)
         
@@ -33,6 +37,7 @@ class GradientDescentNet():
         if isinstance(m, nn.Conv2d) :
             nn.init.xavier_uniform_(m.weight)
             nn.init.constant_(m.bias, 0.001)
+    
     
     def load_checkpoints(self):
         path = os.path.join(self.args.outdir, 'ckpt/epoch'+str(self.args.load)+'.pth')
@@ -43,17 +48,7 @@ class GradientDescentNet():
     
     
     def run_block(self, beta):
-#         print(torch.min(beta), torch.max(beta), torch.mean(beta))
-#         sinogram = self.opr.radon.forward(beta)
-#         print(torch.min(sinogram), torch.max(sinogram), torch.mean(sinogram))
-#         x = self.opr.radon.backprojection(sinogram)
-#         vutils.save_image(x.detach(), f'{self.args.outdir}/test.png', normalize=True)
-        
-#         x = self.opr.forward_gramian(beta)
-#         print(torch.min(x), torch.max(x), torch.mean(x))
-        linear_component = self.eta*self.opr.forward_gramian(beta) + self.network_input
-        linear_component = beta - linear_component
-        
+        linear_component = beta - self.eta*self.opr.forward_gramian(beta) + self.network_input
         regulariser = self.resnet(beta)
         learned_component = -regulariser
         beta = linear_component + learned_component
@@ -66,23 +61,23 @@ class GradientDescentNet():
                 self.resnet.zero_grad()
                 
                 true_sinogram = data[0].to(self.device)
-                true_beta = self.opr.FBP(true_sinogram)  # Ground Truth. Range=(0,1)
+                true_beta = self.opr.FBP(true_sinogram)  # Ground Truth Reconstruction
                 
                 self.network_input = self.opr.forward_adjoint(self.opr.undersample_model(true_sinogram))
                 self.network_input *= self.eta
                 beta = self.network_input
 
-                for _ in range(self.args.blocks):
+                for _ in range(self.args.blocks):  # run iterations
                     beta = self.run_block(beta)
                 
                 self.err = self.criterionL2(beta, true_beta)
                 self.err.backward()
-                self.optimizer.step()
-#                 self.scheduler.step()
+                self.optimizer.step()  # update parameters
+                self.scheduler.step()  # update learning rate
                 
                 if i % 100 == 0:
                     self.log(epoch, i)
-                    
+
             self.log(epoch, i)
             torch.save(self.resnet.state_dict(), f'{self.args.outdir}/ckpt/epoch{epoch}.pth')
             vutils.save_image(beta.detach(), f'{self.args.outdir}/result_samples_epoch{epoch}.png', normalize=True)
@@ -90,7 +85,7 @@ class GradientDescentNet():
             
     def log(self, epoch, i):
         print(f'[{epoch}/{self.args.epochs}][{i}/{len(self.dataloader)}] ' \
-              f'lr:{self.scheduler.get_last_lr()[0]:.4f} ' \
-              f'eta:{self.eta.item():.4f} ' \
+              f'lr:{self.scheduler.get_last_lr()[0]} ' \
+              f'eta:{self.eta.item()} ' \
               f'Loss:{self.err.item():.4f} ' \
              )

utils.py
@@ -1,37 +1,39 @@
 import torch
 import numpy as np
 from torch_radon import Radon
+from torch_radon.solvers import Landweber
 
 
 class Operators():
-    def __init__(self, device):
-        self.image_size = 320
-        self.n_angles = 180
+    def __init__(self, image_size, n_angles, sample_ratio, device):
+        self.device = device
+        self.image_size = image_size
+        self.n_angles = n_angles
         angles = np.linspace(0, np.pi, self.n_angles, endpoint=False)
         self.radon = Radon(self.image_size, angles, clip_to_circle=True)
-        sample_rate = 8
+        self.landweber = Landweber(self.radon)
+        
         self.mask = torch.zeros((1,1,1,180)).to(device)
-        self.mask[:,:,:,::sample_rate].fill_(1)
+        self.mask[:,:,:,::sample_ratio].fill_(1)
         
         
-    # $X^\T ()$  inverse radon with $|X^T|_{op}=1$
+    # $X^\T ()$ inverse radon
     def forward_adjoint(self, input):
         # check dimension
         if input.size()[2] != self.image_size or input.size()[3] != self.n_angles:
             raise Exception(f'forward_adjoint input dimension wrong! received {input.size()}.')
-            
-        return self.radon.backprojection(input.permute(0,1,3,2))//self.n_angles
+        
+        return self.radon.backprojection(input.permute(0,1,3,2))
     
 
-    # $X^\T X ()$ with $|X^T|_{op}=1$
+    # $X^\T X ()$
     def forward_gramian(self, input):
         # check dimension
         if input.size()[2] != self.image_size:
             raise Exception(f'forward_gramian input dimension wrong! received {input.size()}.')
-            
+        
         sinogram = self.radon.forward(input)
-        bp = self.radon.backprojection(sinogram)/self.n_angles
-        return normalize(bp)
+        return self.radon.backprojection(sinogram)
     
 
     # corruption model: undersample sinogram by 8
@@ -45,10 +47,15 @@ class Operators():
         if input.size()[2] != self.image_size or input.size()[3] != self.n_angles:
             raise Exception(f'FBP input dimension wrong! received {input.size()}.')
         filtered_sinogram = self.radon.filter_sinogram(input.permute(0,1,3,2))
-        fbp = self.radon.backprojection(filtered_sinogram)/self.n_angles
-        return normalize(fbp)
+        return self.radon.backprojection(filtered_sinogram)
     
     
+    # estimate step size eta
+    def estimate_eta(self):
+        eta = self.landweber.estimate_alpha(self.image_size, self.device)
+        return torch.tensor(eta, dtype=torch.float32, device=self.device)
+    
+
 def normalize(x, rfrom=None, rto=(0,1)):
     if rfrom is None:
         mean = torch.tensor([torch.min(x),]).cuda()